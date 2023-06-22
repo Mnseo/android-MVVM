b3c2e74eb2f84a21821e8b2ccd2063aee0596189
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-06-22 19:52:43 +0900

    Init: Project Setting

---
+++ app/src/main/java/com/minseo/self_motivation/MyApplication.kt
@@ -0,0 +1,10 @@
+
+package com.minseo.self_motivation
+
+//import android.app.Application
+//import dagger.hilt.android.HiltAndroidApp
+//import kotlin.text.Typography.dagger
+//
+//
+//@HiltAndroidApp
+//class MyApplication : Application()
---
+++ domain/src/main/java/com/minseo/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package com.minseo.domain
+
+class MyClass {
+}
