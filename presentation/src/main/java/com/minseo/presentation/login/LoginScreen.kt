ea23eef832b18db101bb34ff533eaeae96304489
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-07-31 11:00:06 +0900

    Add: Data - Presentation 의존성 추가

---
+++ presentation/src/main/java/com/minseo/presentation/login/LoginScreen.kt
@@ -0,0 +1,17 @@
+package com.minseo.presentation.login
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+
+@Composable
+fun LoginRoute(
+    navigateToMain:() -> Unit,
+    viewModel: LoginViewModel = hiltViewModel()
+) {
+
+}
+@Composable
+fun LoginScreen() {
+
+}
--- presentation/src/main/java/com/minseo/presentation/onboard/OnboardViewModel.kt
+++ presentation/src/main/java/com/minseo/presentation/onboard/OnboardViewModel.kt
@@ -1,13 +1,14 @@
 package com.minseo.presentation.onboard
 
 import androidx.lifecycle.ViewModel
+import com.minseo.data.local.DataStoreManager
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 
 @HiltViewModel
 class OnboardViewModel @Inject constructor(
-
+    private val dataStore: DataStoreManager
 ): ViewModel() {
 
 }
